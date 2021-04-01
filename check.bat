b19aa25c263dbbefb416765df843b05261aa7a54
xuweilun <73899038+xuweilun@users.noreply.github.com>
2021-04-02 03:47:46 +0800

    Update check.bat

--- check.bat
+++ check.bat
@@ -1,10 +1,16 @@
 @echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user administrator THTH-channel /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
+echo To change another VM region, Create New organization (Your current VM location:  %LO% )
+echo Region Available: West Europe, Central US, East Asia, Brazil South, Canada Central, Autralia East, UK South, South India
+echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Rerun failed jobs to get a new RDP.
 net user installer /delete
-curl -o "C:\Users\Public\Desktop\SetupBrowser.zip" https://raw.githubusercontent.com/eweiss2021/Azure_RDP/main/SetupBrowser.zip > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Readme-create-by-TinHocThucHanh.txt" https://raw.githubusercontent.com/xuweilun/ACWIN/main/Readme-create-by-TinHocThucHanh.txt > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\SetupBrowser.zip" https://raw.githubusercontent.com//xuweilun/ACWIN/main/SetupBrowser.zip > out.txt 2>&1
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
